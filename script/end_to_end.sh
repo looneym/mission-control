ad73f88f15651c2528f49ea4832183dbe16c4c91
Micheal Looney <micheal.looney@intercom.io>
2018-12-16 22:54:01 +0000

    Cleanup scripts

--- script/end_to_end.sh
+++ script/end_to_end.sh
@@ -2,26 +2,9 @@
 
 set -e
 
-echo "Building AMI with latest configuration and source code..."
+echo "Building AMI with latest configuration..."
 ./script/build_ami.sh
 echo "Updating Terraform infrastructure with new AMI..."
 ./script/apply_changes.sh
-
-cd infra/main
-ip=$(terraform output --json  | jq .web_instance_public_ips.value[0] | xargs)
-
-echo "Attempting to reach the web_app"
-
-echo "Attempt 1"
-curl $ip
-echo 
-
-sleep 30
-echo "Attempt 2"
-curl $ip
-echo 
-
-sleep 30
-echo "Attempt 3"
-curl $ip
-echo 
+echo "Deploying code to newly built infrastructure..."
+./script/deploy.sh
